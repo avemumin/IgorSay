30792e5f9f32afdfb144b13e562a71122fd75080
avemumin <jacogitproject@gmail.com>
2025-10-17 12:21:17 +0200

    Add project files.

---
+++ IgorSay/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "IgorSay")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "IgorSay.Pages")]
